ec617c442b0a99cf966aef9bec0c526d64d92a46
Ken Wilcox <wilcoxk@gmail.com>
2016-03-27 08:06:57 -0600

    loading up image

--- PlayingWithAnimation/ViewController.swift
+++ PlayingWithAnimation/ViewController.swift
@@ -9,17 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tap: UIButton!
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func tapped(sender: AnyObject) {
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+        print(currentAnimation)
+    }
+    
 }
 
