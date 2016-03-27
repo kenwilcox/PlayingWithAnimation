d7a4c8e187b554e55f09f3af51164332de4576c4
Ken Wilcox <wilcoxk@gmail.com>
2016-03-27 08:35:35 -0600

    using spring dampening

--- PlayingWithAnimation/ViewController.swift
+++ PlayingWithAnimation/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
     @IBAction func tapped(sender: AnyObject) {
         tap.hidden = true
         
-        UIView.animateWithDuration(1, delay: 0, options: [], animations: { 
+        UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: { 
             [unowned self] in
             switch self.currentAnimation {
             case 0:
